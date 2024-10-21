3220c4c705256357f69ac4f772009165a8126555
Emma Palet <emma.palet01@estudiant.upf.edu>
2024-10-21 09:39:15 +0200

    3

--- src/antiprime.py
+++ src/antiprime.py
@@ -35,6 +35,7 @@ def main(x) :
 	## "anti-prime" or "not anti-prime"
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+	x = int(sys.argv[1])
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
